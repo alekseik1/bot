7000681cab6c9559da38aec89c6ab40ad707667a
alekseik1 <alekseik1@yandex.ru>
2017-11-13 18:00:14 +0300

    Meme command

---
+++ commands/mem.py
@@ -0,0 +1,15 @@
+import command_system
+import vkapi
+
+
+def mem():
+   # Получаем случайную картинку из пабли
+   attachment = vkapi.get_random_wall_picture(-145641262)
+   message = 'Picture from stena pablika.\n Ya lyblu memasi'
+   return message, attachment
+
+mem_command = command_system.Command()
+
+mem_command.keys = ['мем', 'мемес', 'боян', 'картинка', 'развлечение', 'удиви меня', 'mem', 'meme']
+mem_command.description = 'Send kartinka with some memes'
+mem_command.process = mem
