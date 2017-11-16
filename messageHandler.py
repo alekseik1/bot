59064a7b2fc2e4ce2f217a8ea18d9fd8ae35cfc5
alekseik1 <alekseik1@yandex.ru>
2017-11-16 15:12:33 +0300

    Debug-print "Sending sorry"

--- messageHandler.py
+++ messageHandler.py
@@ -94,9 +94,10 @@ def create_answer(data, token):
     message, attachment = get_answer(data)
     # Извинимся 1 раз
     do_once = True
-    #if do_once:
-      #  sorry(token)
-       # do_once = False
+    if do_once:
+        print("SENDING SORRY!!!")
+        sorry(token)
+        do_once = False
 
     vkapi.send_message(user_id, token, message, attachment)
 
